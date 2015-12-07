a432628b3868e6b14d60a9f1671520156ad33c3c
mochele15 <michelehaque@umail.ucsb.edu>
2015-12-06 17:18:07 -0800

    Update create.sql to add create aggregate function

--- create.sql
+++ create.sql
@@ -1,6 +1,10 @@
+DROP DATABASE IF EXISTS project;
 CREATE DATABASE project;
 
 USE project;
+
+CREATE AGGREGATE FUNCTION SUM_HE RETURNS STRING SONAME 'SUM_HE.so';
+
 CREATE TABLE Employees (
 	id int,
     age int,
